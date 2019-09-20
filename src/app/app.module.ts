9f9b207444666eaedc8d572bfe702ac8b2388dbf
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-09-20 21:01:37 +0200

    feat: initial github upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/events-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/events-details/events-details.component';
+import { appRoutes } from './routes';
+import { provideForRootGuard } from '@angular/router/src/router_module';
+
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule,
+RouterModule.forRoot(appRoutes)
+  ],
 
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavBarComponent
   ],
-
+providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -9,7 +9,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
       <div class="myEvents">
         <h2>{{ event.name }}</h2>
         <div>Date: {{ event.date }}</div>
-        <div [ngSwitch]="event?.time">Time: {{ event?.time }}
+        <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">Time: {{ event?.time }}
           <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
           <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
           <span *ngSwitchDefault>(Normal Start)</span>
@@ -27,6 +27,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   `,
   styles: [
     `
+    .green { color: #000f1f !important;}
+    .bold { font-weight: bold;}
       .thumbnail {
         min-height: 210px;
       }
@@ -41,4 +43,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass() {
+    const isEarlyStart = this.event && this.event.time === '8:00 am';
+    return { green: isEarlyStart, bold: isEarlyStart };
+  }
 }
