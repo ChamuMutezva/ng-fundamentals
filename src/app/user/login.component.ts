2824c6b835912375399f7128a900bda183efee7a
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2019-10-07 18:15:31 +0200

    angular reactive forms

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,10 +4,13 @@ import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './login.component.html',
+    styles: [`
+    em{float:right; color:#F05C65; padding-left:10px;}`]
 })
 export class LoginComponent {
     username
     password
+    mouseoverLogin
     constructor (private authService:AuthService, private router:Router){
 
     }
